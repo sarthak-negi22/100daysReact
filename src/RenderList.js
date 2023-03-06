7891ad8598a5147f6fdb7ed6b4ee7cf12ae9ccef
Sarthak <saryatnam22@gmail.com>
2023-03-06 23:15:15 +0530

    RenderList completed

--- src/RenderList.js
+++ src/RenderList.js
@@ -75,12 +75,61 @@ function FilterEg() {
     )
 }
 
+function ListKey() {
+    const subjMarks = [{
+
+        sno: 1,
+        uid: 2191,
+        name: 'Tabish Faridi',
+        skill: 'Pro',
+        marks : 98
+    } , 
+
+        {
+            sno: 2,
+            uid: 9398,
+            name: 'Kanishk Soni',
+            skill: 'Ultra Pro',
+            marks: 96
+        
+    } , 
+        {
+            sno: 3,
+            uid: 2204,
+            name: 'Sarthak Negi',
+            skill: 'Super Noob',
+            marks: 86
+        }, 
+        {
+            sno: 4,
+            uid: 10110,
+            name: 'Rohit Soni',
+            skill: 'Guitarist',
+            marks: 76
+        }
+]           
+    const showNewMarks = subjMarks.map(student => {
+        return (
+            <>
+            <li key={ student.sno } >{ student.name }</li>
+            <li>{ student.uid }</li>
+            <li>{ student.marks }</li>
+            <li>{ student.skill }</li>
+            </>
+        );
+    });
+
+    return <ul>{ showNewMarks }</ul>;
+}
+
 export default function RenderList() {
     return (
         <div>
                 {/* <ListEg/> */}
 
-                <FilterEg/>
+                {/* <FilterEg/> */}
+
+                <ListKey/>
         </div>
     );
 }
