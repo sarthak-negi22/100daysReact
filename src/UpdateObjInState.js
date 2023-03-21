ec867c8a4f3879c6f654adc70f285abdb22d3a50
Sarthak <saryatnam22@gmail.com>
2023-03-21 12:37:43 +0530

    update obj in states 50% completed

--- src/UpdateObjInState.js
+++ src/UpdateObjInState.js
@@ -1,5 +1,74 @@
 import { useState } from "react";
 
+// object spread syntax to copy every property separately
+function Form() {
+    const [person, setPerson] = useState({
+        firstName: 'abc',
+        secondName: 'xyz',
+        email: 'abcxyz@gmail.com'
+    });
+
+    function handleFirstName(e) {
+        setPerson({
+            // ...person,                
+            // firstName: e.target.value
+            // is equivalent to
+
+            firstName: e.target.value,
+            secondName: person.lastName,
+            email: person.email
+        });
+    }
+
+    function handleLastName(e) {
+        setPerson({
+            ...person,
+            lastName: e.target.value
+        });
+    }
+
+    function handleEmail(e) {
+        setPerson({
+            ...person,
+            email: e.target.value
+        });
+    }
+    
+    return (
+        <>
+            <label >
+                First Name:
+
+                <input 
+                    value = {person.firstName}
+                    onChange = { handleFirstName }
+                />
+            </label>
+            <label >
+                Last Name:
+
+                <input 
+                    value = {person.lastName}
+                    onChange = { handleLastName }
+                />
+            </label>
+            <label >
+                Email:
+
+                <input 
+                    value = {person.email}
+                    onChange = { handleEmail }
+                />
+            </label>
+            <p>
+                { person.firstName } { person.lastName }  { person.email }
+            </p>
+        </>
+    )
+
+    
+}
+
 function MovingDot() {
     const [position, setPosition] = useState({x : 0, y : 0});
 
@@ -46,7 +115,9 @@ export default function UpdateObjInState() {
         <>
             {/* <h2>Hello world</h2> */}
 
-            <MovingDot/>
+            {/* <MovingDot/> */}
+
+            <Form/>
         </>
     );
 }
