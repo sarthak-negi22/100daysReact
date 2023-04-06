f8c3e28c8891d54c6ddedca9516702c4fb39f15f
Sarthak <saryatnam22@gmail.com>
2023-04-06 15:13:25 +0530

    static version build

--- src/ThinkingInReact.js
+++ src/ThinkingInReact.js
@@ -17,10 +17,10 @@ function ProductCategoryRow({ category }) {
     );
 }
 
-function ProductRow({ products }) { 
-    const name = products.stocked ? products.name : 
+function ProductRow({ product }) { 
+    const name = product.stocked ? product.name : 
     <span style = { { color : 'red' } }>
-        { products.name }
+        { product.name }
     </span>
 
     return (
@@ -30,7 +30,7 @@ function ProductRow({ products }) {
                     { name }
                 </td>
                 <td>
-                    { products.price }
+                    { product.price }
                 </td>
             </tr>
         </>
@@ -38,9 +38,60 @@ function ProductRow({ products }) {
 }
 
 function ProductTable({ products }){
+
+    const rows =[];
+    let lastCategory = null;
+
+    products.forEach((product) => {
+        if(product.category !== lastCategory) {
+            rows.push(
+                <ProductCategoryRow
+                    category = { product.category }
+                    key = { product.category }
+                />
+            );
+        }
+        rows.push(
+            <ProductRow
+                product = { product }
+                key = { product.name }
+            />
+        );
+        lastCategory = product.category;
+    });
+
     return (
         <>
+            <table>
+                <thead>
+                    <tr>
+                        <th>
+                            Name
+                        </th>
+                        <th>
+                            Price
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    { rows }
+                </tbody>
+            </table>
+        </>
+    );
+}
 
+function SearchBar() {
+    return(
+        <>
+           <form >
+            <input type="text" placeholder = "Search" />
+            <label >
+                <input type="checkbox"/>
+                {' '}
+                Only show products in stock
+            </label>
+           </form>
         </>
     );
 }
@@ -48,7 +99,10 @@ function ProductTable({ products }){
 function FilterableProductTable({ products }) {
     return (
         <>
-
+            <SearchBar/>
+            <ProductTable
+                products = { products }
+            />
         </>
     );
 }
