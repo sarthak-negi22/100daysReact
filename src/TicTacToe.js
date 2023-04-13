1bbde822f29ccd8e8ef7c2eeeffeafa6a27c054c
Sarthak <saryatnam22@gmail.com>
2023-04-13 23:58:21 +0530

    50% done

--- src/TicTacToe.js
+++ src/TicTacToe.js
@@ -1,19 +1,15 @@
 import { useState } from 'react';
 
-function Square() {
+function Square({value, onSquareClick}) {
     
-    const [value, setValue] = useState(' ');
+    // const [value, setValue] = useState(null);
 
-    function handleClick() {
-        // console.log('You clicked the square');
-        setValue('X');
-    }
     
     return (
         <>
             <button 
             className = "square"
-            onClick = { handleClick }    
+            onClick = { onSquareClick }    
             >
                 { value }
             </button>
@@ -22,23 +18,60 @@ function Square() {
 }
 
 function Board() {
+    
+    const [squares, setSquares] = useState(Array(9).fill(null));
+
+    function handleClick() {
+        const nextSquares = squares.slice();
+        nextSquares[0] = "X";
+        setSquares(nextSquares);
+      }
+    
+
     return (
         <>
              {/* <button className="square">X</button> */}
              <div className='board-row'>
-                <Square />
-                <Square />
-                <Square />
+                <Square 
+                    value= { squares[0] }
+                    onSquareClick= { handleClick }
+                />
+                <Square 
+                    value= { squares[1] }
+                    onSquareClick= { handleClick }
+                />
+                <Square 
+                    value= { squares[2] }
+                    onSquareClick= { handleClick }
+                />
              </div>
              <div className='board-row'>
-                <Square />
-                <Square />
-                <Square />
+             <Square 
+                    value= { squares[3] }
+                    onSquareClick= { handleClick }
+                />
+                <Square 
+                    value= { squares[4] }
+                    onSquareClick= { handleClick }
+                />
+                <Square 
+                    value= { squares[5] }
+                    onSquareClick= { handleClick }
+                />
              </div>
              <div className='board-row'>
-                <Square />
-                <Square />
-                <Square />
+             <Square 
+                    value= { squares[6] }
+                    onSquareClick= { handleClick }
+                />
+                <Square 
+                    value= { squares[7] }
+                    onSquareClick= { handleClick }
+                />
+                <Square 
+                    value= { squares[8] }
+                    onSquareClick= { handleClick }
+                />
              </div>
         </>
     );
