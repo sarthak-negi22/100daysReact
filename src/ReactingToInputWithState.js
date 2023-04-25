0825656c294a47ed0c930bcc63556f97abb08988
Sarthak <saryatnam22@gmail.com>
2023-04-25 12:15:40 +0530

    reacting to input with state 100% done

--- src/ReactingToInputWithState.js
+++ src/ReactingToInputWithState.js
@@ -1,5 +1,73 @@
 // think about the UI declaratively, not imperatively
 
+import { useState } from "react";
+
+function ImperativeForm() {
+    
+    const [answer, setAnswer] = useState('');
+    const [error, setError] = useState(null);
+    const [status, setStatus] = useState('typing');
+
+    if(status === 'success') {
+        return <h1>That's Corrent!</h1>
+    }
+
+    function submitForm(answer) {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                let incorrectAnswer = answer.toLowerCase() !== 'nahida';
+                if(incorrectAnswer) {
+                    reject(new Error(answer + ' is not the dendro archon!'));
+                }
+                else {
+                    resolve();
+                }
+            }, 1000);
+        });
+    }
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+        setStatus('submitting');
+
+        try {
+            await submitForm(answer);
+            setStatus('success');
+        } catch (err) {
+            setStatus('typing');
+            setError(err);
+        }
+    }
+
+    function handleTextAreaChange(e) {
+        setAnswer(e.target.value);
+    }
+    
+    return (
+        <>
+            <h2>Test Quiz</h2>
+            <p>What is the name of the dendro archon?</p>
+            <form onSubmit = { handleSubmit }>
+                <textarea
+                    value = { answer }
+                    onChange = { handleTextAreaChange }
+                    disabled = { status === 'submitting' }
+                />
+                <br/>
+                <button
+                    disabled = { answer.length === 0 || status === 'submitting' }
+                >
+                    Submit
+                </button>
+                { error != null && 
+                <p classname = 'Error'>
+                    { error.message }
+                </p> }
+            </form>
+        </>
+    );
+}
+
 function DisplayManyVisualStates() {
     
     const visualStaes = [
@@ -60,7 +128,9 @@ export default function ReactToInputWithState() {
 
             {/* <Form/> */}
 
-            <DisplayManyVisualStates/>
+            {/* <DisplayManyVisualStates/> */}
+
+            <ImperativeForm/>
         </>
     );
 }
