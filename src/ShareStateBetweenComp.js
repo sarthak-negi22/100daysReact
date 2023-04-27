6a7b67d289c15f8f4d9f5d16035d03efbb2e7124
Sarthak <saryatnam22@gmail.com>
2023-04-27 15:56:24 +0530

    passed hard coded data from parent comp

--- src/ShareStateBetweenComp.js
+++ src/ShareStateBetweenComp.js
@@ -1,5 +1,84 @@
 import { useState } from "react";
 
+function AddStateToCommonParent() {
+    const [activeIndex, setActiveIndex] = useState(0);
+}
+
+function PassHardCodedDataFromParent() {
+    const [isActive, setIsActive] = useState(false);
+
+    function Panel({ title, children,isActive }) {
+        function handleClick() {
+            setIsActive(true);
+        }
+
+        return (
+            <>
+                <section className = "panel">
+                    <h3>{ title }</h3>
+                    { isActive ? (
+                        <p>{ children }</p>
+                    ) : (
+                        <button onClick = { handleClick }>
+                            Show More
+                        </button>
+                    )}
+                </section>
+            </>
+        );
+    }
+    
+    return (
+        <>
+            <h2>The almighty Raiden Shogun</h2>
+            <Panel title = "About" isActive = { true }>
+                She is the archon of the Inazuma
+            </Panel>
+            <Panel title = "History" isActive = { false }>
+                She took part in the Archons war
+            </Panel>
+        </>
+    );
+}
+
+function RemoveStateFromChildComp() {
+
+    const [isActive, setIsActive] = useState(false);
+
+    function Panel({ title, children,isActive }) {
+        function handleClick() {
+            setIsActive(true);
+        }
+
+        return (
+            <>
+                <section className = "panel">
+                    <h3>{ title }</h3>
+                    { isActive ? (
+                        <p>{ children }</p>
+                    ) : (
+                        <button onClick = { handleClick }>
+                            Show More
+                        </button>
+                    )}
+                </section>
+            </>
+        );
+    }
+    
+    return (
+        <>
+            <h2>The almighty Raiden Shogun</h2>
+            <Panel title = "About">
+                She is the archon of the Inazuma
+            </Panel>
+            <Panel title = "History">
+                She took part in the Archons war
+            </Panel>
+        </>
+    );
+}
+
 function LiftingStateUpEg() {
     
     function Panel({ title, children }) {
@@ -44,7 +123,11 @@ export default function ShareStateBetweenComp() {
         <>
             {/* <h2>Hello world</h2> */}
 
-            <LiftingStateUpEg/>
+            {/* <LiftingStateUpEg/> */}
+
+            {/* <RemoveStateFromChildComp/> */}
+
+            <PassHardCodedDataFromParent/>
         </>
     );  
 }
