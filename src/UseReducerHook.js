7eeef83db4d3abd4200c31f620b1f5a3031bf3ee
Sarthak <saryatnam22@gmail.com>
2023-05-02 12:42:29 +0530

    add text field not workig properly

--- src/UseReducerHook.js
+++ src/UseReducerHook.js
@@ -5,30 +5,146 @@ const initialTasks = [
     {id: 1, text: 'Watch a puppet show', done: false},
     {id: 2, text: 'Lennon Wall pic', done: false},
   ];
+  let nextId = 3;
+function TaskApp() {
+    const [tasks, setTasks] = useState(initialTasks);
 
-function TaskList({ tasks }) {
+  function handleAddTask(text) {
+    setTasks([
+      ...tasks,
+      {
+        id: nextId++,
+        text: text,
+        done: false,
+      },
+    ]);
+  }
+
+  function handleChangeTask(task) {
+    setTasks(
+      tasks.map((t) => {
+        if (t.id === task.id) {
+          return task;
+        } else {
+          return t;
+        }
+      })
+    );
+  }
+
+  function handleDeleteTask(taskId) {
+    setTasks(tasks.filter((t) => t.id !== taskId));
+  }
+
+  return (
+    <>
+      <h1>Prague itinerary</h1>
+      <AddTask onAddTask={handleAddTask} />
+      <TaskList
+        tasks={tasks}
+        onChangeTask={handleChangeTask}
+        onDeleteTask={handleDeleteTask}
+      />
+    </>
+  );
+}
 
 
+function Task({ task, onChange, onDelete }) {
+    const [isEditing, setIsEditing] = useState(false);
+    let taskContent;
+
+    if(isEditing){
+        taskContent = (
+            <>
+                <input
+                    value = { task.text }
+                    onChange = { (e) => {
+                        onChange({
+                            ...task,
+                            text : e.target.value,
+                        });
+                    }}
+                />
+                <button
+                    onClick = { () => setIsEditing(false)}>
+                    Save
+                </button>
+            </>
+        )
+    } else {
+        taskContent = (
+            <>
+                { task.text }
+                <button 
+                    onClick = {() => setIsEditing(true)}>
+                    Edit
+                </button>
+            </>
+        )
+    }
 
     return (
-        <div>
-            { tasks.map((t) => {
+        <>
+            <label >
                 <input
-                    
+                    type = "checkbox"
+                    checked = { task.done }
+                    onChange = { (e) => {
+                        onChange({
+                            ...task,
+                            done : e.target.checked,
+                        });
+                    }}
                 />
-            }) }
-        </div>
+                { taskContent }
+                <button
+                    onClick = { () => onDelete(task.id)}
+                >
+                    Delete
+                </button>
+            </label>
+        </>
+    )
+}
+
+function TaskList({ tasks, onChangeTask, onDeleteTask }) {
+
+    return (
+        <ul>
+            { tasks.map((task) => (
+                <li key = { task.id }>
+                    <Task
+                        task = { task }
+                        onChange = { onChangeTask }
+                        onDelete = { onDeleteTask }
+                    />
+                </li>
+            )) }
+        </ul>    
     );
 }
 
-function AddTask() {
+function AddTask({ onAddText }) {
+    
+    const [text, setText] = useState('');
+
+    function handleChange(e) {
+        setText(e.target.value);
+    }
+    
     return (
         <div>
             <input
-                type = "text"
                 placeholder = "Add text"
+                value = { text }
+                onChange = { (e) => handleChange }
             />{' '}
-            <button>
+            <button 
+                onClick = {() => {
+                    setText('');
+                    onAddText(text);
+                }}>
                 Add
             </button>
         </div>
@@ -40,7 +156,9 @@ export default function UseReducerHook() {
         <>
             {/* <h2>Hello world</h2> */}
 
-            <AddTask/>
+            {/* <AddTask/> */}
+
+            <TaskApp/>
         </>
     );
 }
