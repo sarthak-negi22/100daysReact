1e9d201c2849a79a3faf752acf3f0cf65798749a
Sarthak <saryatnam22@gmail.com>
2023-05-08 16:02:59 +0530

    a reducer function is implemented, step 2

--- src/UseReducerHook.js
+++ src/UseReducerHook.js
@@ -1,8 +1,30 @@
 import { useState } from "react";
+import { useReducer } from "react";
 
 // second step is to write a reducer function
 function taskReducer(tasks,action) {
-    
+    if (action.type === 'added') {
+        return [
+            ...tasks,
+            {
+                id : action.id,
+                text : action.text,
+                done : false,
+            },
+        ];
+    } else if (action.type === 'changed') {
+        return tasks.map((t) => {
+            if(t.id === action.task.id) {
+                return action.task;
+            } else {
+                return t;
+            }
+        });
+    } else if (action.type === 'deleted') {
+        return tasks.filter((t) => t.id != action.id);
+    } else {
+        throw Error('Unknown action:'+action.type);
+    }
 }
 
 
